5103c2296911b6e4c87732eef214d17fd47fb784
guerrieroriccardo <guerrieroriccardo02@gmail.com>
2020-09-03 21:53:01 +0200

    finished resize and added requirements

---
+++ resize.py
@@ -0,0 +1,48 @@
+import glob
+import os
+import sys
+from PIL import Image
+import tkinter as tk
+from tkinter.filedialog import askdirectory
+
+argouments = sys.argv  # 1) width 2) height
+filedirectory = ""
+
+
+def main():
+    tk.Tk().withdraw()
+    setdirectory()
+    modifyfile()
+
+
+def getfile():
+    filename = askdirectory()
+    return filename
+
+
+def setdirectory():
+    global filedirectory
+    filedirectory = getfile()  # get the directory
+    if not os.path.exists(filedirectory + "/output/"):
+        os.makedirs(filedirectory + "/output/")  # create the output directory
+
+
+def modifyfile():
+    # get every files in directory
+    filedirectorys = glob.glob(filedirectory + "/*.png")
+
+    size = (int(argouments[1]), int(argouments[2]))
+
+    i = 1
+    for file in filedirectorys:
+        name = file.split("\\")[1]
+        immagine = Image.open(file)
+        immagine.thumbnail(size, Image.ANTIALIAS)
+        immagine.save(filedirectory + "/output/" +
+                      name, optimize=True, quality=100)
+        completed = int(100/len(filedirectorys)*i)
+        print("Completed: " + str(completed) + "%")
+        i += 1
+
+
+main()
